45c586940a4f78fd09cf66f260e00658c5026c88
aamirxshaikh <aamirshaikh3232@gmail.com>
2024-05-29 23:57:48 +0530

    Initialize Spring Boot project with actuator, data jpa, web, and h2 dependencies

---
+++ src/main/java/com/clinicwave/clinicwaveusermanagementservice/ClinicwaveUserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clinicwave.clinicwaveusermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicwaveUserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicwaveUserManagementServiceApplication.class, args);
+	}
+
+}
