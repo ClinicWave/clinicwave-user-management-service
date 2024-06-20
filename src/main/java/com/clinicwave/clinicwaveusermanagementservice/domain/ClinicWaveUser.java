dae19078808b25800b461a6dcc282db627afda5c
aamirxshaikh <aamirshaikh3232@gmail.com>
2024-06-20 20:50:49 +0530

    Add Domain Classes for ClinicWave User Management Service
    This commit introduces the following changes to the codebase:
    
    - Add `ClinicWaveUser` class: This class represents a user in the ClinicWave system with fields for user information such as name, contact details, and role.
    
    - Add `Permission` class: This class represents a permission in the ClinicWave system with a field for the permission name.
    
    - Add `Role` class: This class represents a role in the ClinicWave system with fields for the role name, role description, and a set of RolePermission objects.
    
    - Add `RolePermission` class: This class represents a role permission in the ClinicWave system with fields for the associated Role and Permission objects.
    
    - Adds `UserType` class: This class represents a user type in the ClinicWave system with a field for the type of user.

---
+++ src/main/java/com/clinicwave/clinicwaveusermanagementservice/domain/ClinicWaveUser.java
@@ -0,0 +1,66 @@
+package com.clinicwave.clinicwaveusermanagementservice.domain;
+
+import com.clinicwave.clinicwaveusermanagementservice.audit.Audit;
+import com.clinicwave.clinicwaveusermanagementservice.enums.GenderEnum;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * This class represents a user in the ClinicWave system.
+ * It extends the Audit class to include audit fields and implements Serializable for ease of use with certain Java frameworks.
+ * It includes several fields related to user information, such as name, contact details, and role.
+ * It is annotated as a JPA Entity, so instances of this class can be automatically persisted in a database.
+ *
+ * @author aamir on 5/29/24
+ */
+@Entity
+@Table(name = "ClinicWaveUser")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClinicWaveUser extends Audit implements Serializable {
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(nullable = false)
+  private String firstName;
+
+  @Column(nullable = false)
+  private String lastName;
+
+  @Column(nullable = false, unique = true)
+  private String mobileNumber;
+
+  @Column(nullable = false, unique = true)
+  private String username;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private LocalDate dateOfBirth;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private GenderEnum gender;
+
+  private String bio;
+
+  private Boolean status;
+
+  @OneToOne
+  private Role role;
+
+  @OneToOne
+  private UserType userType;
+}
---
+++ src/main/java/com/clinicwave/clinicwaveusermanagementservice/domain/RolePermission.java
@@ -0,0 +1,48 @@
+package com.clinicwave.clinicwaveusermanagementservice.domain;
+
+import com.clinicwave.clinicwaveusermanagementservice.audit.Audit;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * This class represents a role permission in the ClinicWave system.
+ * It extends the Audit class to include audit fields and implements Serializable for ease of use with certain Java frameworks.
+ * It includes fields for the associated Role and Permission objects.
+ * It is annotated as a JPA Entity, so instances of this class can be automatically persisted in a database.
+ *
+ * @author aamir on 5/27/24
+ */
+@Entity
+@Table(name = "RolePermission")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RolePermission extends Audit implements Serializable {
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  /**
+   * This field represents the associated Role object.
+   * It is annotated with @ManyToOne to establish a many-to-one relationship with the Role entity.
+   */
+  @ManyToOne
+  private Role role;
+
+  /**
+   * This field represents the associated Permission object.
+   * It is annotated with @ManyToOne to establish a many-to-one relationship with the Permission entity.
+   */
+  @ManyToOne
+  private Permission permission;
+}
