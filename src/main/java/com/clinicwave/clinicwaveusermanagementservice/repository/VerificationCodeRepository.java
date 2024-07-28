92b4abac9dd63b0b2c88003600df6043da887f18
aamirxshaikh <aamirshaikh3232@gmail.com>
2024-07-28 23:11:50 +0530

    Add Methods to ClinicWaveUserRepository and VerificationCodeRepository
    This commit introduces the following changes to the codebase:
    
    - Added `findByEmail` method to `ClinicWaveUserRepository` to find a user by email.
    - Added import for `VerificationCodeTypeEnum` in `VerificationCodeRepository`.
    - Added method `findTopByClinicWaveUserAndTypeOrderByCreatedAtDesc` to `VerificationCodeRepository` to find the most recent verification code by user and type.

--- src/main/java/com/clinicwave/clinicwaveusermanagementservice/repository/VerificationCodeRepository.java
+++ src/main/java/com/clinicwave/clinicwaveusermanagementservice/repository/VerificationCodeRepository.java
@@ -2,9 +2,11 @@ package com.clinicwave.clinicwaveusermanagementservice.repository;
 
 import com.clinicwave.clinicwaveusermanagementservice.domain.ClinicWaveUser;
 import com.clinicwave.clinicwaveusermanagementservice.domain.VerificationCode;
+import com.clinicwave.clinicwaveusermanagementservice.enums.VerificationCodeTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * This interface extends the JpaRepository interface and provides methods to interact with the VerificationCode entity in the database.
@@ -14,4 +16,6 @@ import java.util.List;
  */
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
   List<VerificationCode> findAllByClinicWaveUser(ClinicWaveUser clinicWaveUser);
+
+  Optional<VerificationCode> findTopByClinicWaveUserAndTypeOrderByCreatedAtDesc(ClinicWaveUser clinicWaveUser, VerificationCodeTypeEnum type);
 }
